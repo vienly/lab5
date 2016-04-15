cd91de984c96247205c217141baae056a231ef23
Gin <vienlyt@gmail.com>
2016-04-15 12:39:02 -0700

    Finish problems 1 through 5

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 function sum(n1, n2) {
-  console.log('\"The sum of ' + n1 + ' and ' + n2 + ' is ' + (n1 + n2) + '\"');
+  console.log('\"The sum of ' + n1 + ' and ' + n2 + ' is ' + (n1 + n2) + '.\"');
+}
+
+function multiply(n1, n2) {
+  console.log('\"The product of ' + n1 + ' and ' + n2 + ' is ' + (n1 * n2) + '.\"');
+}
+
+function sumAndMultiply(n1, n2) {
+  console.log('\"The sum of ' + n1 + ' and ' + n2 + ' is ' + (n1 + n2) + '.\"');
+  console.log('\"The numbers ' + n1 + ' and ' + n2 + ' have a product of ' + (n1 + n2) + '.\"');
+}
+
+function sumArray(n) {
+  var size = n.length;
+  var sum = 0;
+  for (var i = 0; i < size; i++) {
+    sum += n[i];
+  }
+  console.log(n.join(" and ") + ' were passed in as an array of number, and ' + sum + ' is their sum.');
+}
+
+function multiplyArray(n) {
+  var size = n.length;
+  var product = 1;
+  for (var i = 0; i < size; i++) {
+    product *= n[i];
+  }
+  console.log(n.join(" and ") + ' were passed in as an array of number, and ' + product + ' is their product.');
 }
